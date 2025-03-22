615854ad84dc494338f766c7976716d266503af9
gdelsolra <gdelsolra@gmail.com>
2025-03-21 23:44:28 -0400

    Complete module 3 with updated travel page, controller, footer partial, and data

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+const trips = require('../../data/trips.json');
+
+
 /** GET travel view */
 const travel = (req, res) => {
-  res.render('travel', { title: 'Travlr Getaways' });
+  res.render('travel', { title: 'Travlr Getaways', trips });
 };
 module.exports = {
     travel
