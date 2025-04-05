e7bf637c282b0b2d7c6f8b28c99eaecf795f410a
gdelsolra <gdelsolra@gmail.com>
2025-04-05 19:43:03 -0400

    Correction of all the errors through the five weeks before starting HW week 5

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
-const trips = require('../../data/trips.json');
-
+var fs = require('fs'); 
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 
+'utf8')); 
 
 /** GET travel view */
 const travel = (req, res) => {
--- app_server/models/seed.js
+++ app_server/models/seed.js
@@ -1,10 +1,12 @@
+// Bring in the DB connection and the Trip schema
 const Mongoose = require('./db');
 const Trip = require('./travlr');
+
+// Read seed data from json file
 const fs = require('fs');
-const path = require('path');
 
 // Read seed data using path.join for safety
-const trips = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/trips.json'), 'utf8'));
+const trips = JSON.parse(fs.readFileSync('./data/trips.json'), 'utf8');
 
 const seedDB = async () => {
   try {
